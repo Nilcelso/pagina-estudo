e0197b7f279794f15f53df84519510423e8e86ce
Nilcelso Sobrinho <nilcelsop@gmail.com>
2020-07-21 14:55:07 -0300

    dados

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE",
+        featured: true,
+    },
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE"
+    },
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE",
+        featured: true,
+    },
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE"
+    },
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE",
+        featured: true,
+    },
+    {
+        id: "b1uDnGcqeeQ",
+        title: "Video aula sobre JavaScript",
+        duration: "67 min",
+        price: "FREE"
+    },
+]
